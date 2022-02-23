58dc262bf527866f89af5a6ed0284f7e2e51ec19
Billy <billy@dwenola.com>
2022-02-23 13:54:25 +0100

    feat(layout): add title base and update padding logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
 <body>
 
 <div class="w-64 bg-gray-800 h-full fixed top-0 bottom-0 left-0">
-    <div class="text-center pb-3 pt-3 mb-3 border-b border-gray-500">
+    <div class="text-center pb-4 pt-4 mb-3 border-b border-gray-500">
         <span class="text-2xl text-white font-bold text-center">moneyar</span>
     </div>
     <a href="#" class="p-3 m-3 rounded text-white  block hover:bg-gray-600">
@@ -22,13 +22,13 @@
     <a href="#" class="p-3 m-3 rounded text-white  block hover:bg-gray-600">
         <i class="fa fa-chart-line mr-3"></i> Projections
     </a>
-    <a href="#" class="p-3 m-3 rounded text-white  block hover:bg-gray-600">
+    <a href="/settings/banks" class="p-3 m-3 rounded text-white  block hover:bg-gray-600">
         <i class="fa fa-wrench mr-3"></i> Configuration
     </a>
 </div>
 
 <main class="pl-64">
-    <div class="container mx-auto p-3">
+    <div class="container mx-auto p-4">
         @yield('content')
     </div>
 </main>
---
+++ resources/views/part/page_title.blade.php
@@ -0,0 +1,3 @@
+<div class="text-2xl font-bold pb-4 border-b border-gray-200 mb-4">
+    {!! $title !!}
+</div>
